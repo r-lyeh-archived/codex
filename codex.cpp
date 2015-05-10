312fae9c77c7da9eb8a9cd2eb0813db4b583c1e7
r-lyeh <rlyeh.nospam@gmail.com>
2015-05-10 17:13:19 +0200

    Delete .gitignore

--- codex.cpp
+++ codex.cpp
@@ -1,5 +1,5 @@
 /* Simple escaping/unescaping/encoding/decoding functions.
- * Copyright (c) 2013 Mario 'rlyeh' Rodriguez
+ * - rlyeh, zlib/libpng licensed
 
  * Based on code from escape-utils by Brian Lopez
  * Copyright (c) 2010-2011 Brian Lopez - http://github.com/brianmario
@@ -10,29 +10,8 @@
  * Based on code from dfa by Bjoern Hoehrmann
  * Copyright (c) 2008-2010 Bjoern Hoehrmann - http://bjoern.hoehrmann.de/utf-8/decoder/dfa/
 
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
  * To do:
  * - [...]
-
- * - rlyeh
  */
 
 #include <cassert>
